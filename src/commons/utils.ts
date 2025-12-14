204088c9299b9911096f03ce02938ccaada1f592
bonatolasa <tolboni614@gmail.com>
2025-12-14 19:13:21 +0300

    first commit

---
+++ src/commons/utils.ts
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken';
+
+export class CommonUtils {
+    static generateReferralCode(length = 6): string {
+        const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+        let code = "";
+        for (let i = 0; i < length; i++) {
+            code += chars.charAt(Math.floor(Math.random() * chars.length));
+        }
+        return code.toLocaleUpperCase();
+    }
+
+    //jwt
+    static generateJwtToken(jwtData){
+        const generatedToken=jwt.sign(jwtData,process.env.JWT_SECRET!,{
+            expiresIn:"10m"
+        })
+        return generatedToken;
+    }
+
+}
