4e69dd3b7e3640e7accec3ddeb2243908a3ced0c
bonatolasa <tolboni614@gmail.com>
2025-12-15 20:33:45 +0300

    referral commit

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,15 +3,19 @@ import { UsersController } from './controllers/users.controller';
 import { UsersService } from './services/users.service';
 import { MongooseModule } from '@nestjs/mongoose';
 import { userSchema } from './schemas/users.schema';
+import { referralSchema } from 'src/referals/schemas/referrals.schema';
+import { ReferralService } from 'src/referals/services/referals.service';
 
 @Module({
     imports:[
         MongooseModule.forFeature([
-           { name:'User', schema: userSchema}
+           { name:'User', schema: userSchema},
+           {name:'Referral', schema: referralSchema}
         ])
-    ],
-    
+    ], 
     controllers:[UsersController],
-    providers:[UsersService]
+    providers:[UsersService,
+        ReferralService
+    ]
 })
 export class UsersModule {}
