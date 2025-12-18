8ae18330c9a15758e552eb5ca9a54bad4ce086bc
bonatolasa <tolboni614@gmail.com>
2025-12-18 19:33:38 +0300

    referrals response

---
+++ src/referals/responses/referals.response.ts
@@ -0,0 +1,13 @@
+ export class ReferrerResponse{
+      id?:string
+      referrerId?:string;
+      referrerFullName?:string;
+      referrerUsername?:string;
+    }
+
+export class ReferredUserResponse{
+        id?:string;
+        referredUserId?:string;
+        referredUserFullName?:string
+
+}
