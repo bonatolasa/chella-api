ee05d697b6fc01b9781ec00cfc711ccd9e88b7b0
bonatolasa <tolboni614@gmail.com>
2025-12-20 22:33:28 +0300

    task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,4 +1,17 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { TaskSchema, UserTasksSchema } from './schemas/tasks.schema';
+import { TasksService } from './services/tasks.service';
+import { TasksController } from './controllers/tasks.controller';
 
-@Module({})
+@Module({
+      imports:[
+            MongooseModule.forFeature([
+               { name:'Task', schema: TaskSchema},
+               {name:'UserTask', schema: UserTasksSchema}
+            ])
+        ], 
+        controllers:[TasksController],
+        providers:[TasksService]
+})
 export class TasksModule {}
