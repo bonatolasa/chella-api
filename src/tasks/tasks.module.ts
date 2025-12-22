502cea89b4f0080c4f62a8f58173c31ffab106fc
bonatolasa <tolboni614@gmail.com>
2025-12-22 21:54:45 +0300

    task module

--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,17 +1,28 @@
 import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
-import { TaskSchema, UserTasksSchema } from './schemas/tasks.schema';
+import { Task, taskSchema, UserTask, UserTasksSchema } from './schemas/tasks.schema';
 import { TasksService } from './services/tasks.service';
 import { TasksController } from './controllers/tasks.controller';
+import { UsersService } from 'src/users/services/users.service';
+import { User, userSchema } from 'src/users/schemas/users.schema';
+import { Referral, referralSchema } from 'src/referals/schemas/referrals.schema';
+import { ReferralService } from 'src/referals/services/referals.service';
 
 @Module({
       imports:[
             MongooseModule.forFeature([
-               { name:'Task', schema: TaskSchema},
-               {name:'UserTask', schema: UserTasksSchema}
+               {name:Task.name, schema: taskSchema},
+               {name:UserTask.name, schema: UserTasksSchema},
+               {name:User.name, schema: userSchema},
+               {name:Referral.name, schema:referralSchema}
+               
             ])
         ], 
         controllers:[TasksController],
-        providers:[TasksService]
+        providers:[
+            TasksService,
+            UsersService,
+            ReferralService
+        ]
 })
 export class TasksModule {}
