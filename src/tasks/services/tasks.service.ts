7dc0154214c26c3feb3c6ef1a1f8787005d12605
bonatolasa <tolboni614@gmail.com>
2025-12-23 20:01:30 +0300

    task service

--- src/tasks/services/tasks.service.ts
+++ src/tasks/services/tasks.service.ts
@@ -3,8 +3,9 @@ import { InjectModel } from "@nestjs/mongoose";
 import { Model } from "mongoose";
 import { Task, UserTask } from "../schemas/tasks.schema";
 import { Cron, CronExpression } from "@nestjs/schedule";
-import { TaskResponse, UserTaskResponse } from "../responses/tasks.response";
+import { TaskResponse } from "../responses/tasks.response";
 import { response } from "express";
+import { UsersService } from "src/users/services/users.service";
 
 @Injectable()
 export class TasksService{
@@ -12,7 +13,9 @@ export class TasksService{
         @InjectModel(Task.name)
         private readonly taskModel:Model<Task>,
         @InjectModel(UserTask.name)
-        private readonly userTaskModel:Model<UserTask>
+        private readonly userTaskModel:Model<UserTask>,
+
+        private readonly userService:UsersService
     ){}
 
     //Background job to create daily tasks
@@ -62,7 +65,7 @@ export class TasksService{
     }
 
     //update completed tasks
-    async completeTask(userId:string,taskId:string){
+    async completeTask(currentUser,taskId:string){
         
         //check if task exists
         const task = await this.taskModel.findById(taskId)
@@ -72,36 +75,59 @@ export class TasksService{
         }
         
         //check if user has already completed the task today
-        const today=new Date().toISOString().split('T')[0]
-        const existingUserTask=await this.userTaskModel.findOne({
-            userId,
-            taskId,
-            taskDate:today
+        const alreadyCompleatedExists=await this.userTaskModel.exists({
+            userId:currentUser.id,
+            taskId:taskId,
         })
 
-        if(existingUserTask){
+        if(alreadyCompleatedExists){
             throw new BadRequestException("Task already completed today");
         }
+        //creating new userTask instance and saving to db
+        const userTask = await this.userTaskModel.create({
+           userId:currentUser.id,
+           taskId,
+           isCompleted:true
+         });
 
-        //mark task as completed
-        const userTask=new this.userTaskModel({
-            userId,
-            taskId,
-            taskDate:today,
-            rewardAmount:task.rewardAmount
-        })
-        await userTask.save()
+         const savedUserTask = await userTask.save()
+
+         //updating user totalEarned amount
+         const updatedUser=await this.userService.addTaskRewardToUser(currentUser.id, task.rewardAmount);
+
+         //use intercepter and respond reward(field)
+         const response:TaskResponse={
+            id:task._id.toString(),
+            rewardAmount:task.rewardAmount,
+            isCompleted:savedUserTask.isCompleted,
+            taskDate:task.taskDate
+         }
+
+      return response;
     }
 
-    // fetch completed tasks for a user
-    async getUserCompletedTasks(userId:string){
+    // service to fetch completed tasks by current user
+  async getUserCompletedTasks(currentUser){
+    //1. get user completed tasks from userTask collection
+    const userTasks = await this.userTaskModel.find({ 
+        userId: currentUser.id, 
+        isCompleted: true });
+    console.log("USER TASKS:", userTasks);
 
-         const userTasks=await this.userTaskModel.find({userId})
+    const taskIds= userTasks.map(userTask => userTask.taskId);
+    
+    const tasks=await this.taskModel.find({
+        _id:{$in:taskIds}
+    })
 
-         const response:UserTaskResponse[]=userTasks.map(userTask=>({
-            id:userTask._id.toString(),
-            taskId:userTask.taskId,
-        }))
-        return response
-    }
+    //3. preparing response
+    const response:TaskResponse[]=tasks.map((task)=>({
+        id:task._id.toString(),
+        title:task.title,
+        rewardAmount:task.rewardAmount,
+        taskDate:task.taskDate,
+        isCompleted:true
+    }))
+    return response;
+  } 
 }
