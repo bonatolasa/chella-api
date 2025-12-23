44ca0a34b02a257e6c78df5df15233d95bb510e9
bonatolasa <tolboni614@gmail.com>
2025-12-23 20:01:42 +0300

    user service

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -5,12 +5,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 import { userSchema } from './schemas/users.schema';
 import { referralSchema } from 'src/referals/schemas/referrals.schema';
 import { ReferralService } from 'src/referals/services/referals.service';
+import { UserTasksSchema } from 'src/tasks/schemas/tasks.schema';
+import { TasksService } from 'src/tasks/services/tasks.service';
 
 @Module({
     imports:[
         MongooseModule.forFeature([
            { name:'User', schema: userSchema},
-           {name:'Referral', schema: referralSchema}
+           {name:'Referral', schema: referralSchema},
         ])
     ], 
     controllers:[UsersController],
