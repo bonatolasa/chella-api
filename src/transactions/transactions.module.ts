c75107e0bb0a159d00d4597fad8406a1c6e62afc
bonatolasa <tolboni614@gmail.com>
2025-12-28 19:14:18 +0300

    transaction module

--- src/transactions/transactions.module.ts
+++ src/transactions/transactions.module.ts
@@ -1,4 +1,78 @@
-import { Module } from '@nestjs/common';
+import { Module } from "@nestjs/common";
+import { MongooseModule } from "@nestjs/mongoose";
+import { Transaction, transactionSchema } from "./schemas/transaction.schema";
+import { TransactionsService } from "./services/transactions.service";
+import { TransactionsController } from "./controllers/transactions.controller";
+import { User, userSchema } from "src/users/schemas/users.schema";
 
-@Module({})
+@Module({
+  imports:[
+    MongooseModule.forFeature([
+      {name:Transaction.name, schema:transactionSchema},
+      {name:User.name, schema:userSchema}
+    ])
+  ],
+  controllers:[
+    TransactionsController
+  ],
+  providers:[
+    TransactionsService
+  ]
+})
+export class TransactionsModule{}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Transaction, TransactionSchema } from './schemas/transaction.schema';
+import { User, userSchema } from 'src/users/schemas/users.schema';
+import { ExchangeRate, ExchangeRateSchema } from 'src/exchange-rates/schemas/exchage-rates.schema';
+import { TransactionsController } from './controllers/transactions.controller';
+import { TransactionsService } from './services/transactions.service';
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Transaction.name, schema: TransactionSchema },
+      { name: User.name, schema: userSchema },
+      { name: ExchangeRate.name, schema: ExchangeRateSchema },
+    ]),
+  ],
+  controllers: [TransactionsController],
+  providers: [TransactionsService],
+})
 export class TransactionsModule {}
+*/
