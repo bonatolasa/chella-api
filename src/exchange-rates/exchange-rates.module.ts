b8e5364c3b48efa0d7ac43d00d21a6544038f991
bonatolasa <tolboni614@gmail.com>
2026-01-01 19:27:15 +0300

    exchange rates modules

--- src/exchange-rates/exchange-rates.module.ts
+++ src/exchange-rates/exchange-rates.module.ts
@@ -1,4 +1,27 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 
-@Module({})
+import { HttpModule } from '@nestjs/axios';
+import { ExchangeRate, ExchangeRateSchema } from './schemas/exchage-rates.schema';
+import { ExchangeRatesService } from './services/exchange-rates.service';
+import { ExchangeRatesController } from './controllers/exchange-rates.controller';
+
+
+
+@Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: ExchangeRate.name, schema: ExchangeRateSchema },
+    ]),
+    HttpModule
+  ],
+  controllers:[
+    ExchangeRatesController
+  ],
+  providers:[
+    ExchangeRatesService
+  ],
+
+
+})
 export class ExchangeRatesModule {}
